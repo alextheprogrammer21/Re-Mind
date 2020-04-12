55874035b075f2335d90c559ac443443a68518a8
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-04-12 16:51:36 -0700

    Navbar added

---
+++ react-front-end/src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Navigation() {
+  return (
+    <Navbar bg="light">
+      <Container>
+        <Navbar.Brand href="#home">re:mind</Navbar.Brand>
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <Button variant="outline-primary">Log In</Button>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
--- react-front-end/src/stories/app.stories.js
+++ react-front-end/src/stories/app.stories.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import MyButton from "../components/Button";
+import Navigation from "../components/Navigation";
 
 storiesOf("MyButton", module)
   .add("Primary", () => <MyButton type="primary" text="Click me!"></MyButton>)
   .add("Secondary", () => (
     <MyButton type="secondary" text="Click me!"></MyButton>
   ));
+storiesOf("Navbar", module).add("Navbar", () => <Navigation></Navigation>);
