35844ef95f1339c6827f25ab96e7343847ec802d
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-04-12 18:02:16 -0700

    Promo card added

---
+++ react-front-end/src/components/Promo.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
+import yoga from "../img/yoga.jpg";
+import "./Promo.css";
+const classNames = require("classnames");
+
+export default function Promo(props) {
+  const cardClasses = classNames({
+    "text-right": props.side === "right",
+    "text-left": props.side === "left",
+    "text-white": props.light,
+  });
+
+  return (
+    <>
+      <Card className={cardClasses}>
+        <Card.Img src={yoga} className={Promo} />
+        <Card.ImgOverlay>
+          <Card.Title>{props.header || "Placeholder"}</Card.Title>
+          <Card.Text>{props.content || "Card Body"}</Card.Text>
+        </Card.ImgOverlay>
+      </Card>
+    </>
+  );
+}
--- react-front-end/src/stories/app.stories.js
+++ react-front-end/src/stories/app.stories.js
@@ -2,6 +2,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import MyButton from "../components/Button";
 import Navigation from "../components/Navigation";
+import Promo from "../components/Promo";
 
 storiesOf("MyButton", module)
   .add("Primary", () => <MyButton type="primary" text="Click me!"></MyButton>)
@@ -9,3 +10,20 @@ storiesOf("MyButton", module)
     <MyButton type="secondary" text="Click me!"></MyButton>
   ));
 storiesOf("Navbar", module).add("Navbar", () => <Navigation></Navigation>);
+storiesOf("Promo", module)
+  .add("Empty Promo", () => <Promo></Promo>)
+  .add("Left Light Text", () => (
+    <Promo
+      side="left"
+      light
+      header="Build Healthy Habits Fast!"
+      content="Our app was designed to enable you to quickly build healthy habits"
+    ></Promo>
+  ))
+  .add("Right Dark Text", () => (
+    <Promo
+      side="right"
+      header="Build Healthy Habits Fast!"
+      content="Our app was designed to enable you to quickly build healthy habits"
+    ></Promo>
+  ));
