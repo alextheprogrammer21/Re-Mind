57dbf0f08944966f27b72cf51f2f073ebfdcc98b
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-04-15 12:48:11 -0700

    Calendar query fixed

--- express-back-end/db/queries.js
+++ express-back-end/db/queries.js
@@ -66,7 +66,7 @@ const getDashboard = (id, cb) => {
 const calendar = (data) => {
   let curr = new Date();
   let week = [];
-  for (let i = 0; i <= 6; i++) {
+  for (let i = 1; i <= 7; i++) {
     let first = curr.getDate() - curr.getDay() + i;
     let day = new Date(curr.setDate(first)).toISOString().slice(0, 10);
     week.push({ day: day, plan: [] });
