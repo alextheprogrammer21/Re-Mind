df3618d5d4d8d19112ac5626f8cf2ee5a642f7d9
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-04-16 18:07:45 -0700

    SMS sending working

--- express-back-end/send_reminder.js
+++ express-back-end/send_reminder.js
@@ -1,16 +1,20 @@
-// Setup
+// DB Connection Setup
 require("dotenv").config();
 const { Pool } = require("pg");
 const dbParams = require("./db/db.js");
 const db = new Pool(dbParams);
 db.connect();
 
-// Send SMS to those notifications
+// Twilio Setup
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+
 // Mark them complete
 // Run the script every 10 mins with cron
 
 // Script to query the database and see if there are pending notfications
-const getPending = () => {
+const getPending = (cb) => {
   db.query(
     `select n.*, h.user_id, u.first_name, u.phone, a.name from notifications n
     join habits h on habit_id = h.id
@@ -19,11 +23,25 @@ const getPending = () => {
     where scheduled_time > current_timestamp - interval '720 minutes' and scheduled_time < current_timestamp + interval '720 minutes'`
   )
     .then((data) => {
-      console.log("Returned rows: ", data.rows);
-      console.log("Returned count: ", data.rowCount);
-      return data.rows;
+      cb(data.rows);
     })
     .catch((err) => console.log(err));
 };
 
-const sendSms = () => {};
+// Send SMS to those notifications
+const sendSms = () => {
+  getPending((result) => {
+    result.forEach((element) => {
+      console.log(element);
+      client.messages
+        .create({
+          body: `Hey ${element.first_name}. Reminder to do ${element.name} from re:mind! Have you completed it?`,
+          from: "+19893751056",
+          to: `${element.phone}`,
+        })
+        .then((message) => console.log(message.sid));
+    });
+  });
+};
+
+sendSms();
