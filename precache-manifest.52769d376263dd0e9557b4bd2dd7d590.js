b9d976cdfd63642822d178e66d3879fd006e0455
Alex S <alextheprogrammer21@gmail.com>
2020-06-22 16:16:36 -0700

    Updates

---
+++ precache-manifest.52769d376263dd0e9557b4bd2dd7d590.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "563b57c6d276cf9cc283",
+    "url": "/Re-Mind/static/css/main.b1588c4b.chunk.css"
+  },
+  {
+    "revision": "563b57c6d276cf9cc283",
+    "url": "/Re-Mind/static/js/main.849e9fa1.chunk.js"
+  },
+  {
+    "revision": "50139f73ae97e2a2a97b",
+    "url": "/Re-Mind/static/js/runtime~main.354dccb0.js"
+  },
+  {
+    "revision": "d0571d2403e53f22ea34",
+    "url": "/Re-Mind/static/css/2.db278149.chunk.css"
+  },
+  {
+    "revision": "d0571d2403e53f22ea34",
+    "url": "/Re-Mind/static/js/2.5f8dc880.chunk.js"
+  },
+  {
+    "revision": "f22bfb5c889496835fdfa90ccedf1c00",
+    "url": "/Re-Mind/index.html"
+  }
+];
